5d8259ad49491cd23aa779dab56392b092923b68
git-a-h <146676192+git-a-h@users.noreply.github.com>
2025-11-25 21:13:15 +0100

    add lint to notebook.py

--- src/step_3/notebook.py
+++ src/step_3/notebook.py
@@ -1,9 +1,11 @@
 # %% [markdown]
-# Welcome to this notebook!
-# In this step, this notebook will guide you how to use the add function from step_1
-# First, we need to define / import the add function.
+## Welcome to this notebook! <br>
+# In this step, this notebook will guide you how to use the add function from step_1 <br>
+# First, we import all of the necessary packages. <br>
+# This is also nice to see that $\lambda$ works in markdown cell.
 
 # %%
+import matplotlib.pyplot as plt
 import os
 import sys
 
@@ -12,8 +14,7 @@ root = os.path.abspath(os.path.join(cwd, "..", ".."))
 if root not in sys.path:
     sys.path.append(root)
 
-# %%
-from src.step_1.dummy_add import add
+from src.step_1.dummy_add import add  # noqa: E402
 
 # %% [markdown]
 # To use the add function, simply call it via add(a,b) where a and b are of the type int, float or complex.
@@ -25,17 +26,15 @@ add(2, 3)
 # The output of the cell above should be 5
 
 # %% [markdown]
-# Using this type of notebook, you can easily create plots as well
+# Using this type of notebook, you can easily create plots as well.
 
 # %%
-import matplotlib.pyplot as plt
-
 list = [add(i, i) for i in range(10)]
 plt.plot(list)
 plt.show()
 
 
 # %% [markdown]
-# This is just a docs on how to create simple notebooks using Jupytext.
-# jupytext --to ipynb --execute path/to/notebook.py
-# jupyter notebook path/to/notebook.ipynb
+# This is just a docs on how to create simple notebooks using Jupytext. <br>
+# jupytext --to ipynb --execute path/to/notebook.py <br>
+# jupyter notebook path/to/notebook.ipynb <br>
